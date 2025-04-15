fd09a24b8b306ad4fa0bb07c51f710e8daaa16b6
Huradil <nuradiltolokov@gmail.com>
2025-04-15 10:19:28 +0600

    add fix to websockets

--- config/websocket.py
+++ config/websocket.py
@@ -5,30 +5,39 @@ active_connections = defaultdict(set)
 
 
 async def websocket_application(scope, receive, send):
-    if scope["type"] == "websocket.connect":
-        await send({"type": "websocket.accept"})
+    if scope["type"] == "websocket":
+        room_name = "notifications"
+        room_group_name = f"group_{room_name}"
 
-    room_name = "notifications"
-    room_group_name = f"group_{room_name}"
+        active_connections[room_group_name].add(send)
+        try:
+            await send({"type": "websocket.accept"})
 
-    active_connections[room_group_name].add(send)
+            while True:
+                event = await receive()
 
-    while True:
-        event = await receive()
+                if event["type"] == "websocket.disconnect":
+                    break
 
-        # if event["type"] == "websocket.connect":
-        #     await send({"type": "websocket.accept"})
-
-        if event["type"] == "websocket.disconnect":
-            active_connections[room_group_name].remove(send)
-            break
-
-        if event["type"] == "websocket.receive":
-            if event["text"] == "ping":
-                await send({"type": "websocket.send", "text": "pong!"})
-    active_connections[room_group_name].remove(send)
+                if event["type"] == "websocket.receive":
+                    if event.get("text") == "ping":
+                        await send({"type": "websocket.send", "text": "pong!"})
+        finally:
+            active_connections[room_group_name].discard(send)
 
 
 async def broadcast_message(message: str):
-    for connection in active_connections["group_notifications"]:
-        await connection({"type": "websocket.send", "text": json.dumps({"messages": message})})
+    room_group_name = "group_notifications"
+    dead_connections = set()
+
+    for connection in active_connections[room_group_name]:
+        try:
+            await connection({
+                "type": "websocket.send",
+                "text": json.dumps({"messages": message})
+            })
+        except Exception as e:
+            dead_connections.add(connection)
+
+    for conn in dead_connections:
+        active_connections[room_group_name].discard(conn)
